d240c83b2c19fb2dbf4a4f47a13d203884d55c68
irishh04 <139461236+irishh04@users.noreply.github.com>
2024-04-01 10:06:00 +0800

    Updated_Ver
    Updated_Version

--- Angular-ntt/src/app/app.component.ts
+++ Angular-ntt/src/app/app.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LatestNewsComponent } from './components/latest-news/latest-news.component';
 import { MainComponent } from './components/main/main.component';
+import { TheTeamComponent } from './components/the-team/the-team.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent, MainComponent, LatestNewsComponent, TheTeamComponent, ContactComponent, FooterComponent],
+  templateUrl: './app.component.html' ,
+  styleUrl: './app.component.css',
+  schemas : [CUSTOM_ELEMENTS_SCHEMA],
 })
 
 export class AppComponent {
   title = 'Angular-ntt';
-
-
 }
 
 
--- Angular-ntt/tailwind.config.js
+++ Angular-ntt/tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js}', // Specify the files you want Tailwind to analyze
-    './public/index.html',   // Add other relevant file paths
+    './src/**/*.{html,js}',
+    './public/index.html',   
+    './src/**/*.jsx',
   ],
   theme: {
-    extend: {colors:{"custom-primary":"#00C6B3", "custom-accent":"#686995", "custom-accent-hover":"#445A7E","custom-bg": "#111827", "custom-bg-light" : "#244A64", "custom-text": "#EBFBFF"}},
+    extend: {colors:{"custom-primary":"#FE7A36", "custom-accent":"#686995", "custom-accent-hover":"#445A7E","custom-bg": "#111827", "custom-bg-light" : "#244A64", "custom-text": "#EBFBFF"}},
+    
   },
   plugins: [
-    require('taos/plugin')
   ],
   safelist: [
     '!duration-[0ms]',
     '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
   ]
 }
 
